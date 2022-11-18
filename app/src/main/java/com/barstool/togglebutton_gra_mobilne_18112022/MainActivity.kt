f7f90fb70e2a1de9c5abd95bee8017dbfb861e1c
OGMaksiu <mateckimaksymilian@gmail.com>
2022-11-18 14:44:52 +0100

    Initial commit

---
+++ app/src/main/java/com/barstool/togglebutton_gra_mobilne_18112022/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.barstool.togglebutton_gra_mobilne_18112022
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
